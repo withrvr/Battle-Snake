7de6e2f4f3db98eaac91e246902339581a0002ea
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-02-02 20:29:39 +0000

    just move snake         anti-click-wise from the edges

--- server.py
+++ server.py
@@ -1,63 +1,100 @@
 import os
 import random
-
 import cherrypy
 
-"""
-This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
-"""
-
-
 class Battlesnake(object):
+
+    def __init__(self):
+        # board height and width
+        self.board_height = 0
+        self.board_width = 0
+
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
+    def index(self):        
+        
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "author": "withrvr",
+            "color": "#6441a5", # twitch color code
+            "head": "silly",
+            "tail": "skinny",
         }
+
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        data = cherrypy.request.json
 
-        print("START")
+        # setting the size
+        self.board_height = data["board"]["height"] - 1
+        self.board_width  = data["board"]["width"]  - 1
+        self.terminate_turn = (2 * (self.board_height + self.board_width))
+
+        print()
+        print(board_height)
+        print(board_width)
+        print(terminate_turn)
+        print()
+
+        print("\nSTART\n")
         return "ok"
+
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
+        """
+        self current code
+        just move snake
+        anti-click-wise from the edges
+
+        more advance in development
+
+        """
+
         data = cherrypy.request.json
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+        # default move of the snake
+        move = 'left'
 
-        print(f"MOVE: {move}")
+        # head x and y value of snake
+        head_x = data["you"]["head"]["x"]
+        head_y = data["you"]["head"]["y"]
+
+        
+        # checking the corner of the board
+
+        # bottom-left corner
+        if head_x == 0 and head_y == 0:
+            move = 'right'
+        # bottom-right corner
+        elif head_x == 0 and head_y == self.board_width:
+            move = 'up'
+        # top-right corner
+        elif head_x == self.board_height and head_y == self.board_width:
+            move = 'left'
+        # top-left corner
+        elif head_x == 0 and head_y == self.board_width:
+            move = 'down'
+
+
+        # foo-foo-foo code to terminate the process
+
+        # if data["turn"] > self.terminate_turn:
+        #     move = ''
+
+        print(f"\nMOVE: {move}\n")
         return {"move": move}
+
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def end(self):
-        # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
-        data = cherrypy.request.json
-
-        print("END")
+        
+        print("\nEND\n")
         return "ok"
 
 
@@ -67,5 +104,5 @@ if __name__ == "__main__":
     cherrypy.config.update(
         {"server.socket_port": int(os.environ.get("PORT", "8080")),}
     )
-    print("Starting Battlesnake Server...")
+    print("\nStarting Battlesnake Server...\n")
     cherrypy.quickstart(server)
